3ee7a7d240008579e6a4c7ae652483d41ef67304
Nahid Parvez <nahid.parvez@bjitacademy.com>
2023-10-31 15:33:30 +0600

    aws bucket setup

--- routes/File.js
+++ routes/File.js
@@ -4,29 +4,37 @@ const upload = require("../config/files");
 const FileController = require("../controller/FileController");
 const { MulterError } = require("multer");
 const { sendResponse } = require("../util/common");
+const {
+    S3Client,
+    PutObjectCommand,
+} = require("@aws-sdk/client-s3");
+const { AWS_REGION, S3_BUCKET, S3_BASE_URL } = process.env;
 
-routes.post(
-    "/upload-file",
-    upload.single("file_to_upload"),
-    // (req, res, next) => {
-    //     console.log(req.file);
-    //     if (!req.file) {
-    //         return sendResponse(res, 404, "File too large, or file not found");
-    //     }
-    //     return sendResponse(res, 200, "Successfully uploaded file");
-    //     // if (!req.file) {
-    //     //     return sendResponse(res, 413, "File not found");
-    //     // }
-    //     // if (err) {
-    //     //     console.log(err instanceof MulterError);
-    //     //     console.log(err);
-    //     //     return sendResponse(res, 413, "File too large");
-    //     // } else {
-    //     //     // next();
-    //     // }
-    // }
-    FileController.uploadFile
-);
-// routes.post("/create", isAuthenticated, isAdmin, productValidator.add, ProductController.create);
+const uploadFile = async function (file) {
+    const s3Client = new S3Client({ region: AWS_REGION });
+    const key = `nahid-bucket-folder/${Date.now()}-${file.originalname}`;
+
+    const s3Params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: key,
+        Body: file.buffer,
+    };
+    console.log("s3 params: ", s3Params)
+    await s3Client.send(new PutObjectCommand(s3Params));
+    return `${s3Params.Key}`;
+};
+
+routes.post("/upload-file", upload.single("file"), async (req, res) => {
+    try {
+        const file = req.file;
+        const response = await uploadFile(file); // Await the uploadFile function
+
+        res.send(response);
+    } catch (error) {
+        // Handle errors here
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 module.exports = routes;
